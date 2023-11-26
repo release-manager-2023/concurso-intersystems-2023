0911723c1103d85f394da38a9b81217d81a19e46
Higor Granzoto <higorrg@gmail.com>
2023-11-26 17:13:46 -0300

    Setup liquibase

--- backend/src/main/java/br/com/releasemanger/VersionStatus.java
+++ backend/src/main/java/br/com/releasemanger/VersionStatus.java
@@ -1,7 +1,10 @@
 package br.com.releasemanger;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,8 +13,12 @@ import lombok.EqualsAndHashCode;
 @Table(schema = "RELEASE_MANAGER", name = "VERSION_STATUS")
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class VersionStatus extends PanacheEntity {
+public class VersionStatus extends PanacheEntityBase {
 
+	@Id
+	@GeneratedValue(generator = "native", strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
 	private String name;
 
 }
