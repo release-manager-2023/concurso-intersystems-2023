03b1a685a0b873856b0b4614942cbe2056d30172
Higor Granzoto <higorrg@gmail.com>
2023-11-26 17:01:54 -0300

    Setup liquibase

--- backend/src/main/java/br/com/releasemanger/App.java
+++ backend/src/main/java/br/com/releasemanger/App.java
@@ -8,10 +8,6 @@ public class App {
 
 	public static void main(String... args) {
 		System.out.println("Running main method");
-//		IRISDatabase database = (IRISDatabase) DatabaseFactory.getInstance().openDatabase(url, null, null, null, null);
-//		Liquibase liquibase = new Liquibase("liquibase/ext/changelog.generic.test.xml",
-//				new ClassLoaderResourceAccessor(), database);
-//		liquibase.update("");
 		Quarkus.run(args);
 	}
 }
