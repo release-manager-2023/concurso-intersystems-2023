b88f7611ce0a27013564b8d13b4aabb366cd258e
Higor Granzoto <higorrg@gmail.com>
2023-11-26 18:47:50 -0300

    US-003: Backend Stakeholders Status Version

--- backend/src/main/java/br/com/releasemanger/stakeholder/model/entity/Stakeholder.java
+++ backend/src/main/java/br/com/releasemanger/stakeholder/model/entity/Stakeholder.java
@@ -1,11 +1,17 @@
 package br.com.releasemanger.stakeholder.model.entity;
 
+import java.util.List;
+
+import br.com.releasemanger.version_status.model.entity.VersionStatus;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -28,4 +34,7 @@ public class Stakeholder extends PanacheEntityBase {
 	private String stakeholderRole;
 	@Email
 	private String email;
+	@ManyToMany
+	@JoinTable(schema = "RELEASE_MANAGER", name = "STAKEHOLDER_STATUSVERSION", joinColumns = @JoinColumn(name = "stakeholder_id"), inverseJoinColumns = @JoinColumn(name = "status_version_id"))
+	private List<VersionStatus> versionStatuses;
 }
--- backend/src/main/resources/db/changelog-v1.sql
+++ backend/src/main/resources/db/changelog-v1.sql
@@ -2,7 +2,7 @@
 
 --changeset higor:1
 create table if not exists RELEASE_MANAGER.VERSION_STATUS (
- id int not null primary key auto_increment,
+ id bigint not null primary key auto_increment,
  name varchar(60) not null
 );
 
@@ -14,9 +14,16 @@ insert into RELEASE_MANAGER.VERSION_STATUS (NAME) values ('General Availability'
 insert into RELEASE_MANAGER.VERSION_STATUS (NAME) values ('Deprecated');
 
 --changeset higor:3
-create table RELEASE_MANAGER.STAKEHOLDER (
-  id int not null primary key auto_increment,
+create table if not exists RELEASE_MANAGER.STAKEHOLDER (
+  id bigint not null primary key auto_increment,
   name varchar(60) not null,
   stakeholder_role varchar(60),
   email varchar(100)
+);
+
+--changeset higor:4
+create table if not exists RELEASE_MANAGER.STAKEHOLDER_STATUSVERSION (
+  stakeholder_id bigint not null references RELEASE_MANAGER.STAKEHOLDER(id),
+  status_version_id bigint not null references RELEASE_MANAGER.VERSION_STATUS(id),
+  primary key (stakeholder_id,status_version_id)
 );
