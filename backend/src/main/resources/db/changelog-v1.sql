a91f7ec018e39fdced6a4df209d8f85ea2f59cd3
Higor Granzoto <higorrg@gmail.com>
2023-11-26 14:20:29 -0300

    Setup liquibase

---
+++ backend/src/main/resources/db/changelog-v1.sql
@@ -0,0 +1,14 @@
+--liquibase formatted sql
+
+--changeset higor:1
+create table if not exists RELEASE_MANAGER.VERSION_STATUS (
+ id int not null primary key auto_increment,
+ name varchar(60) not null
+);
+
+--changeset higor:2
+insert into RELEASE_MANAGER.VERSION_STATUS (NAME) values ('Internal');
+insert into RELEASE_MANAGER.VERSION_STATUS (NAME) values ('Canary');
+insert into RELEASE_MANAGER.VERSION_STATUS (NAME) values ('General Availability');
+insert into RELEASE_MANAGER.VERSION_STATUS (NAME) values ('Revoked');
+insert into RELEASE_MANAGER.VERSION_STATUS (NAME) values ('Deprecated');
